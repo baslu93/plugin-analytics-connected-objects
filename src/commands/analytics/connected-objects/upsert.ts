fc4255d1b810b3976df7e2b4a03bdb95d965ba4e
Luca Bassani <lbassani@lbassani-ltmjn5j.internal.salesforce.com>
2026-06-16 12:13:31 +0200

    fix: removing extra comma

--- src/commands/analytics/connected-objects/upsert.ts
+++ src/commands/analytics/connected-objects/upsert.ts
@@ -47,7 +47,7 @@ export default class ConnectedObjectsUpsert extends SfCommand<ConnectedObjectUps
       exclusive: ['recipe-names'],
     }),
     verbose: Flags.boolean({
-      summary: messages.getMessage('flags.verbose.summary'),
+      summary: messages.getMessage('flags.verbose.summary')
     })
   };
 
